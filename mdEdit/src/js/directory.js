9f666e4726cce24ba10bd214101e8e578569c158
sliyingjie_v <sliyingjie_v@didichuxing.com>
2022-07-13 09:13:38 +0800

    comit

--- mdEdit/src/js/directory.js
+++ mdEdit/src/js/directory.js
@@ -38,5 +38,52 @@
       e.target.classList.add('checked')
     }
     document.body.appendChild(directoryContent)
+
+
+    const fullScreen = document.createElement('div')
+    fullScreen.innerHTML = '+'
+    fullScreen.className = 'fullScreen'
+    fullScreen.onclick = function() {
+      if (!window.fullScreen) {
+        window.fullScreen = true
+        fullScreen.innerHTML = '-'
+        if(document.documentElement.RequestFullScreen){
+          document.documentElement.RequestFullScreen();
+        }
+        //兼容火狐
+        console.log(document.documentElement.mozRequestFullScreen)
+        if(document.documentElement.mozRequestFullScreen){
+          document.documentElement.mozRequestFullScreen();
+        }
+        //兼容谷歌等可以webkitRequestFullScreen也可以webkitRequestFullscreen
+        if(document.documentElement.webkitRequestFullScreen){
+          document.documentElement.webkitRequestFullScreen();
+        }
+        //兼容IE,只能写msRequestFullscreen
+        if(document.documentElement.msRequestFullscreen){
+          document.documentElement.msRequestFullscreen();
+        }
+      } else {
+        window.fullScreen = false
+        fullScreen.innerHTML = '+'
+        if(document.exitFullScreen){
+          document.exitFullscreen()
+        }
+        //兼容火狐
+        console.log(document.mozExitFullScreen)
+        if(document.mozCancelFullScreen){
+          document.mozCancelFullScreen()
+        }
+        //兼容谷歌等
+        if(document.webkitExitFullscreen){
+          document.webkitExitFullscreen()
+        }
+        //兼容IE
+        if(document.msExitFullscreen){
+          document.msExitFullscreen()
+        }
+      }
+    }
+    document.body.appendChild(fullScreen)
   }, 0)
 })()
